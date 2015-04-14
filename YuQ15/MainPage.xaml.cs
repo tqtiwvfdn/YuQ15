d805d6eb93fe4bbe0ae6f97fc567db5efc720c98
黎健成 <594610825@qq.com>
2015-04-14 13:31:42 +0800

    YuQ15-高校运动会内容发布系统-WP8.1客户端

---
+++ YuQ15/MainPage.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO;
+using Microsoft.Phone.Tasks;
+using System.IO.IsolatedStorage;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+
+namespace YuQ15
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // 主页的 Url
+        private string MainUri;
+        private string imgStr;
+        PhotoChooserTask myTask;
+
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //图片选择器
+            myTask = new PhotoChooserTask();
+            myTask.Completed += MyTask_Completed;
+
+            imgStr = (Application.Current as App).backgroundImage;
+            if (string.IsNullOrEmpty(imgStr))
+            {
+                ImageMenuItem_Click(null, null);
+            }
+            else
+            {
+                setBg(imgStr);
+            }
+        }
+
+        private void Browser_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            MainUri = (Application.Current as App).HostPath;
+
+            if (string.IsNullOrEmpty(MainUri))
+            {
+                try
+                {
+                    this.NavigationService.Navigate(new Uri("/Pages/SetHostPathPage.xaml", UriKind.Relative));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                // 在此处添加 URL
+                Browser.Navigate(new Uri(MainUri, UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void Browser_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            Browser.InvokeScript("eval", "document.body.style.backgroundImage='url("+ MainUri + "/Images/bg.jpg)';");
+            Browser.Opacity = 1;
+        }
+
+        // 在 Web 浏览器的导航堆栈而不是应用程序中向后导航。
+        private void BackApplicationBar_Click(object sender, EventArgs e)
+        {
+            Browser.GoBack();
+        }
+
+        // 在 Web 浏览器的导航堆栈而不是应用程序中向前导航。
+        private void ForwardApplicationBar_Click(object sender, EventArgs e)
+        {
+            Browser.GoForward();
+        }
+
+        // 导航到初始“主页”。
+        private void HomeMenuItem_Click(object sender, EventArgs e)
+        {
+            Browser.Navigate(new Uri(MainUri, UriKind.RelativeOrAbsolute));
+        }
+
+        // 处理导航故障。
+        private void Browser_NavigationFailed(object sender, System.Windows.Navigation.NavigationFailedEventArgs e)
+        {
+            MessageBox.Show("无法导航到此页面，请检查 Internet 连接");
+            this.NavigationService.Navigate(new Uri("/Pages/SetHostPathPage.xaml", UriKind.Relative));
+        }
+
+        private void RefreshBarIconButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Browser.InvokeScript("eval", "history.go()");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Browser_Navigating(object sender, NavigatingEventArgs e)
+        {
+            Browser.Opacity = 0;
+        }
+
+
+
+        public static String GetImageStr(String imgFilePath)
+        {
+            string type;
+            switch (imgFilePath.Substring(imgFilePath.LastIndexOf('.') + 1))
+            {
+                case "jpg":
+                case "jpeg":
+                    type = "JPEG";
+                    break;
+                case "bmp":
+                    type = "BITMAP";
+                    break;
+                default:
+                    type = "PNG";
+                    break;
+            }
+            // 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+            byte[] data = null;
+            // 读取图片字节数组
+            try
+            {
+                FileStream fileStream = new FileStream(imgFilePath, FileMode.Open, FileAccess.Read);
+                data = new byte[fileStream.Length];
+                fileStream.Read(data, 0, data.Length);
+                fileStream.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            // 对字节数组Base64编码
+            return "data:image/" + type + ";base64," + Convert.ToBase64String(data);
+        }
+
+        //设置网站地址
+        private void SettingMenuItem_Click(object sender, EventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/Pages/SetHostPathPage.xaml", UriKind.Relative));
+        }
+
+        //设置背景图
+        private void ImageMenuItem_Click(object sender, EventArgs e)
+        {
+            if (myTask != null)
+            {
+                myTask.Show();
+            }
+        }
+
+        private void MyTask_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                IsolatedStorageSettings mySetting = IsolatedStorageSettings.ApplicationSettings;
+                mySetting["backgroundImage"] = e.OriginalFileName;
+                PhoneApplicationService.Current.State["backgroundImage"] = e.OriginalFileName;
+                (Application.Current as App).backgroundImage = e.OriginalFileName;
+                mySetting.Save();
+                setBg(e.OriginalFileName);
+            }
+        }
+        private void setBg(string imgStr)
+        {
+            ImageBrush imgbr = new ImageBrush();
+            BitmapImage img = new BitmapImage(new Uri(imgStr, UriKind.RelativeOrAbsolute));
+            imgbr.ImageSource = img;
+            LayoutRoot.Background = imgbr;
+        }
+    }
+}
---
+++ YuQ15/Pages/SetHostPathPage.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace YuQ15.Pages
+{
+    public class HostPathItem
+    {
+        public HostPathItem(String hostPathString) { this.HostPathString = hostPathString; }
+        public String HostPathString { get; set; }
+    }
+
+    public partial class SetHostPathPage : PhoneApplicationPage
+    {
+        public string newHostPathString = null;
+        private string imgStr;
+        public SetHostPathPage()
+        {
+            InitializeComponent();
+
+            GetHostPathList();
+
+            imgStr = (Application.Current as App).backgroundImage;
+            if (!string.IsNullOrEmpty(imgStr))
+            {
+                setBg(imgStr);
+            }
+        }
+
+        private void GetHostPathList()
+        {
+            List<HostPathItem> myHostPathItems = new List<HostPathItem>();
+            IsolatedStorageSettings mySettings = IsolatedStorageSettings.ApplicationSettings;
+
+            foreach (var x in mySettings)
+            {
+                if (x.Key != "HostPath"&&x.Key!= "backgroundImage")
+                    myHostPathItems.Add(new HostPathItem((string)x.Value));
+            }
+            this.hostPathList.ItemsSource = myHostPathItems;
+        }
+
+        //清理缓存
+        private void CleanHostPathCache(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("确定要删除网站地址缓存吗？", "羽晴 | YuQ11", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                IsolatedStorageSettings.ApplicationSettings.Clear();
+                
+                //保存背景图片
+                IsolatedStorageSettings mySetting = IsolatedStorageSettings.ApplicationSettings;
+                mySetting["backgroundImage"] = PhoneApplicationService.Current.State["backgroundImage"] as string;
+                mySetting.Save();
+
+                GetHostPathList();
+            }
+        }
+
+        //清除提示
+        private void ClearTips(object sender, RoutedEventArgs e)
+        {
+            TextBox myTextBox = e.OriginalSource as TextBox;
+            if (myTextBox.Text == "示例：192.168.1.2/YuQ11")
+            {
+                myTextBox.Text = string.Empty;
+                myTextBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+        //增加提示
+        private void AddTips(object sender, RoutedEventArgs e)
+        {
+            TextBox myTextBox = e.OriginalSource as TextBox;
+            if (string.IsNullOrEmpty(myTextBox.Text))
+            {
+                myTextBox.Text = "示例：192.168.1.2/YuQ11";
+                myTextBox.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+        //增加网站地址
+        private void EnterToInsertTheNewPath(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                InsertTheNewHostPath(null, null);
+            }
+        }
+        private void InsertTheNewHostPath(object sender, RoutedEventArgs e)
+        {
+            string newHostPath = hostPathTextBox.Text.Trim().ToLower().Replace(@"\", "/");
+            if (newHostPath[newHostPath.Length - 1] == '/')
+            {
+                newHostPath = newHostPath.Substring(0, newHostPath.Length - 1);
+            }
+            if (!newHostPath.Contains("http://"))
+            {
+                newHostPath = "http://" + newHostPath;
+            }
+
+            //测试是否可行
+            newHostPathString = newHostPath;
+            try
+            {
+                WebClient myWebClient = new WebClient();
+                myWebClient.DownloadStringCompleted += TestTheHostPathAvaluable;
+                myWebClient.Encoding = System.Text.Encoding.UTF8;
+                myWebClient.DownloadStringAsync(new Uri(newHostPath + "/Static/isYuQ11.html?t="+(new Random(DateTime.Now.Millisecond)).Next()));
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "羽晴 | YuQ11", MessageBoxButton.OK);
+                this.hostPathTextBox.Focus();
+            }
+        }
+        //测试结果
+        private void TestTheHostPathAvaluable(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null||e.Result!="yes")
+            {
+                MessageBox.Show("网站地址有误，请重新输入", "羽晴 | YuQ11", MessageBoxButton.OK);
+                this.hostPathTextBox.Focus();
+            }
+            else
+            {
+                IsolatedStorageSettings mySetting = IsolatedStorageSettings.ApplicationSettings;
+                if (mySetting.Count(p => (string)p.Value == newHostPathString) <= 0)
+                {
+                    int i = IsolatedStorageSettings.ApplicationSettings.Count;
+                    string key = "Path" + i.ToString();
+                    mySetting.Add(key, newHostPathString);
+                    mySetting.Save();
+                }
+                ChangeHostPath();
+            }
+        }
+
+        //选中地址
+        private void HostPathList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LongListSelector myLongListSelector = sender as LongListSelector;
+            HostPathItem myHostPathItem = myLongListSelector.SelectedItem as HostPathItem;
+            newHostPathString = myHostPathItem.HostPathString;
+
+            ChangeHostPath();
+        }
+        //更改地址
+        private void ChangeHostPath()
+        {
+            IsolatedStorageSettings mySetting = IsolatedStorageSettings.ApplicationSettings;
+            mySetting["HostPath"] = newHostPathString;
+            mySetting.Save();
+            (Application.Current as App).HostPath = newHostPathString;
+
+            MessageBox.Show("设置网站地址成功！");
+            this.NavigationService.GoBack();
+        }
+
+        private void setBg(string imgStr)
+        {
+            ImageBrush imgbr = new ImageBrush();
+            BitmapImage img = new BitmapImage(new Uri(imgStr, UriKind.RelativeOrAbsolute));
+            imgbr.ImageSource = img;
+            imgbr.Opacity = .45;
+            LayoutRoot.Background = imgbr;
+        }
+    }
+}
